79bb0789f61567a21c8f6d29349c8a9919af9a31
sxr1223 <1173368667@qq.com>
2021-07-20 11:22:37 +0800

    2021_7_20 	多通道数据采集和求和取平均，单极性SPWM，每周期4000点，中值1020，幅值21000 开关频率100K，TIM17为0.05ms 	在过零处还是存在失真的情况，需要对比不同正弦表的效果，不同中值，幅值，是否完全对称，产生完全对称的后半周程序在VS的temp工程中。 	现阶段数据量太大，DMA中断耗时太长，导致其他中断执行延迟。这个问题虽然可以通过优先级来调整，但终究不是最好的办法，因为现在这样程序进入mian函数的while循环时间比较短，没有办法保证循环内执行是否及时，只能放一些时间要求不高的代码，比如说显示之类的。说白了，还是334的主频太低了。
    主函数内想sys_tic来定时执行一些代码，但是根本无法完成，很容易错过去。时间要求稍高的代码还是得靠中断。

---
+++ Core/Src/main.c
@@ -0,0 +1,415 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "comp.h"
+#include "dac.h"
+#include "dma.h"
+#include "hrtim.h"
+#include "hrtim.h"
+#include "i2c.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "oled.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#define SPWM_NUM 400
+#define DATA_LEN 100
+#define DATA_CH_NUM 6
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+static uint16_t spwm[]=
+{
+1020,1349,1679,2009,2338,
+2667,2996,3324,3651,3978,
+4305,4630,4955,5278,5601,
+5922,6242,6561,6878,7194,
+7509,7822,8133,8442,8750,
+9056,9360,9661,9961,10258,
+10553,10846,11136,11424,11709,
+11992,12272,12549,12823,13095,
+13363,13628,13891,14150,14405,
+14658,14907,15153,15395,15634,
+15869,16100,16328,16552,16772,
+16988,17200,17409,17613,17813,
+18009,18201,18388,18571,18750,
+18925,19095,19261,19422,19579,
+19731,19878,20021,20159,20292,
+20421,20545,20664,20778,20887,
+20992,21091,21186,21275,21360,
+21439,21514,21583,21648,21707,
+21761,21810,21854,21893,21926,
+21955,21978,21996,22009,22017,
+
+
+22017,22009,21996,21978,21955,
+21926,21893,21854,21810,21761,
+21707,21648,21583,21514,21439,
+21360,21275,21186,21091,20992,
+20887,20778,20664,20545,20421,
+20292,20159,20021,19878,19731,
+19579,19422,19261,19095,18925,
+18750,18571,18388,18201,18009,
+17813,17613,17409,17200,16988,
+16772,16552,16328,16100,15869,
+15634,15395,15153,14907,14658,
+14405,14150,13891,13628,13363,
+13095,12823,12549,12272,11992,
+11709,11424,11136,10846,10553,
+10258,9961,9661,9360,9056,
+8750,8442,8133,7822,7509,
+7194,6878,6561,6242,5922,
+5601,5278,4955,4630,4305,
+3978,3651,3324,2996,2667,
+2338,2009,1679,1349,1020
+};
+//max duty 91.2
+//medium 1020
+//amplitude ?
+
+uint16_t adc_data[DATA_LEN][DATA_CH_NUM]={0};
+uint16_t adc_data_fin[DATA_CH_NUM]={0};
+float adc_ch1_amp=0;
+
+uint32_t sys_tic=0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+void set_PWM(uint16_t duty,uint8_t ch)
+{
+	hhrtim1.Instance->sTimerxRegs[ch].CMP1xR = duty;
+	hhrtim1.Instance->sTimerxRegs[ch].CMP2xR = duty+(23404-duty)/2;
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	static uint16_t spwm_index=0;
+	static uint8_t sys_tic_tim=0;
+	
+	if(htim==(&htim17))
+	{
+		if(spwm_index<200)
+		{
+			set_PWM(spwm[spwm_index],0);
+			if(spwm_index==0)
+			{
+				HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_RESET);
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_SET);
+			}
+		}
+		if(spwm_index>=200)
+		{
+			set_PWM(23040-spwm[spwm_index-200],0);
+			if(spwm_index==200)
+			{
+				HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_RESET);
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				__NOP();
+				HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_SET);
+			}
+		}
+		spwm_index++;
+		if(spwm_index==400)
+			spwm_index=0;
+		
+		sys_tic_tim++;
+		if(sys_tic_tim==20)
+		{
+			sys_tic++;
+			sys_tic_tim=0;
+		}
+	}
+}
+
+uint16_t adc_ch1_max=0;
+uint16_t adc_ch1_min=4096;
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+
+	uint16_t i,j;
+	uint32_t sum;
+	static float scale=510.0f/2.0f;
+	
+	if(hadc==&hadc1)
+	{
+		for(i=0;i<DATA_CH_NUM;i++)
+		{
+			sum=0;
+			for(j=0;j<DATA_LEN;j++)
+				sum+=adc_data[j][i];
+			adc_data_fin[i]=sum/DATA_LEN;
+		}
+		
+		if(adc_data_fin[0]>adc_ch1_max)
+		{
+			adc_ch1_max=adc_data_fin[0];
+			adc_ch1_amp=(adc_ch1_max-adc_ch1_min)/4096.0f*3.3f/20*scale;
+		}
+		if(adc_data_fin[0]<adc_ch1_min)
+		{
+			adc_ch1_min=adc_data_fin[0];
+			adc_ch1_amp=(adc_ch1_max-adc_ch1_min)/4096.0f*3.3f/20*scale;
+		}		
+		
+		HAL_ADC_Start_DMA(&hadc1,(uint32_t*)adc_data,DATA_LEN*DATA_CH_NUM);
+	}
+}
+
+void HAL_COMP_TriggerCallback(COMP_HandleTypeDef *hcomp)
+{
+
+}
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_HRTIM1_Init();
+  MX_TIM17_Init();
+  MX_ADC1_Init();
+  MX_COMP2_Init();
+  MX_DAC1_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+  
+	HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_MASTER);
+	
+	HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TA1|HRTIM_OUTPUT_TA2);
+	HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_A);
+	
+//	HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TB1|HRTIM_OUTPUT_TB2);
+//	HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_B);
+//	
+//	HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TC1|HRTIM_OUTPUT_TC2);
+//	HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_C);
+//	
+//	HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TD1|HRTIM_OUTPUT_TD2);
+//	HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_D);
+
+	HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_RESET);
+	
+	HAL_TIM_Base_Start_IT(&htim17);
+	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)adc_data,DATA_LEN*DATA_CH_NUM);
+	
+	OLED_Init();
+	OLED_Display_On();
+	OLED_printf(0,0,16,"Hello World.");
+	HAL_COMP_Start_IT(&hcomp2);
+	//HAL_COMP_Start_IT(&hcomp2);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  if(sys_tic%100==0)
+	  {
+		  adc_ch1_max=0;
+		  adc_ch1_min=4096;
+	  }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_HRTIM1|RCC_PERIPHCLK_USART1
+                              |RCC_PERIPHCLK_I2C1|RCC_PERIPHCLK_ADC12;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  PeriphClkInit.Adc12ClockSelection = RCC_ADC12PLLCLK_DIV1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  PeriphClkInit.Hrtim1ClockSelection = RCC_HRTIM1CLK_PLLCLK;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Drivers/PID/pid.c
@@ -0,0 +1,198 @@
+/******************************************************************************
+/// @brief
+/// @copyright Copyright (c) 2017 <dji-innovations, Corp. RM Dept.>
+/// @license MIT License
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction,including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense,and/or sell
+/// copies of the Software, and to permit persons to whom the Software is furnished
+/// to do so,subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+*******************************************************************************/
+/**
+  ******************************************************************************
+  * @file			pid.c
+  * @version		V1.0.0
+  * @date			2016年11月11日17:21:36
+  * @brief   		对于PID， 反馈/测量习惯性叫get/measure/real/fdb,
+						  期望输入一般叫set/target/ref
+  *******************************************************************************/
+  
+  
+  
+/* Includes ------------------------------------------------------------------*/
+#include "pid.h"
+#include "mytype.h"
+#include <math.h>
+//#include "cmsis_os.h"
+
+#define ABS(x)		((x>0)? (x): (-x)) 
+
+void abs_limit(float *a, float ABS_MAX){
+    if(*a > ABS_MAX)
+        *a = ABS_MAX;
+    if(*a < -ABS_MAX)
+        *a = -ABS_MAX;
+}
+
+
+
+void pid_test_init(){
+	
+
+	//为了解决上位机调参的时候第一次赋值的时候清零其他参数， 应该提前把参数表填充一下！
+
+}
+/**
+  ****************************(C) COPYRIGHT 2019 DJI****************************
+  * @file       pid.c/h
+  * @brief      pid实现函数，包括初始化，PID计算函数，
+  * @note       
+  * @history
+  *  Version    Date            Author          Modification
+  *  V1.0.0     Dec-26-2018     RM              1. 完成
+  *
+  @verbatim
+  ==============================================================================
+
+  ==============================================================================
+  @endverbatim
+  ****************************(C) COPYRIGHT 2019 DJI****************************
+  */
+
+#include "pid.h"
+#include "main.h"
+
+#define LimitMax(input, max)   \
+    {                          \
+        if (input > max)       \
+        {                      \
+            input = max;       \
+        }                      \
+        else if (input < -max) \
+        {                      \
+            input = -max;      \
+        }                      \
+    }
+
+/**
+  * @brief          pid struct data init
+  * @param[out]     pid: PID struct data point
+  * @param[in]      mode: PID_POSITION: normal pid
+  *                 PID_DELTA: delta pid
+  * @param[in]      PID: 0: kp, 1: ki, 2:kd
+  * @param[in]      max_out: pid max out
+  * @param[in]      max_iout: pid max iout
+  * @retval         none
+  */
+/**
+  * @brief          pid struct data init
+  * @param[out]     pid: PID结构数据指针
+  * @param[in]      mode: PID_POSITION:普通PID
+  *                 PID_DELTA: 差分PID
+  * @param[in]      PID: 0: kp, 1: ki, 2:kd
+  * @param[in]      max_out: pid最大输出
+  * @param[in]      max_iout: pid最大积分输出
+  * @retval         none
+  */
+void PID_init(pid_type_def *pid, uint8_t mode, const fp32 PID[3], fp32 max_out, fp32 max_iout)
+{
+    if (pid == NULL || PID == NULL)
+    {
+        return;
+    }
+    pid->mode = mode;
+    pid->Kp = PID[0];
+    pid->Ki = PID[1];
+    pid->Kd = PID[2];
+    pid->max_out = max_out;
+    pid->max_iout = max_iout;
+    pid->Dbuf[0] = pid->Dbuf[1] = pid->Dbuf[2] = 0.0f;
+    pid->error[0] = pid->error[1] = pid->error[2] = pid->Pout = pid->Iout = pid->Dout = pid->out = 0.0f;
+}
+
+/**
+  * @brief          pid calculate 
+  * @param[out]     pid: PID struct data point
+  * @param[in]      ref: feedback data 
+  * @param[in]      set: set point
+  * @retval         pid out
+  */
+/**
+  * @brief          pid计算
+  * @param[out]     pid: PID结构数据指针
+  * @param[in]      ref: 反馈数据
+  * @param[in]      set: 设定值
+  * @retval         pid输出
+  */
+fp32 PID_calc(pid_type_def *pid, fp32 ref, fp32 set)
+{
+    if (pid == NULL)
+    {
+        return 0.0f;
+    }
+
+    pid->error[2] = pid->error[1];
+    pid->error[1] = pid->error[0];
+    pid->set = set;
+    pid->fdb = ref;
+    pid->error[0] = set - ref;
+    if (pid->mode == PID_POSITION)   //位置式
+    {
+        pid->Pout = pid->Kp * pid->error[0];
+        pid->Iout += pid->Ki * pid->error[0];
+        pid->Dbuf[2] = pid->Dbuf[1];
+        pid->Dbuf[1] = pid->Dbuf[0];
+        pid->Dbuf[0] = (pid->error[0] - pid->error[1]);
+        pid->Dout = pid->Kd * pid->Dbuf[0];
+        LimitMax(pid->Iout, pid->max_iout);
+        pid->out = pid->Pout + pid->Iout + pid->Dout;
+        LimitMax(pid->out, pid->max_out);
+    }
+    else if (pid->mode == PID_DELTA)    //增量式
+    {
+        pid->Pout = pid->Kp * (pid->error[0] - pid->error[1]);
+        pid->Iout = pid->Ki * pid->error[0];
+        pid->Dbuf[2] = pid->Dbuf[1];
+        pid->Dbuf[1] = pid->Dbuf[0];
+        pid->Dbuf[0] = (pid->error[0] - 2.0f * pid->error[1] + pid->error[2]);
+        pid->Dout = pid->Kd * pid->Dbuf[0];
+        pid->out += pid->Pout + pid->Iout + pid->Dout;
+        LimitMax(pid->out, pid->max_out);
+    }
+    return pid->out;
+}
+
+/**
+  * @brief          pid out clear
+  * @param[out]     pid: PID struct data point
+  * @retval         none
+  */
+/**
+  * @brief          pid 输出清除
+  * @param[out]     pid: PID结构数据指针
+  * @retval         none
+  */
+void PID_clear(pid_type_def *pid)
+{
+    if (pid == NULL)
+    {
+        return;
+    }
+
+    pid->error[0] = pid->error[1] = pid->error[2] = 0.0f;
+    pid->Dbuf[0] = pid->Dbuf[1] = pid->Dbuf[2] = 0.0f;
+    pid->out = pid->Pout = pid->Iout = pid->Dout = 0.0f;
+    pid->fdb = pid->set = 0.0f;
+}
