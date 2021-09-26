9fd532758f9f4b81139820259eb70419624189f4
sxr1223 <1173368667@qq.com>
2021-09-26 16:26:38 +0800

    添加必要文件，下次应该及时add

---
+++ Core/Src/comp.c
@@ -0,0 +1,148 @@
+/**
+  ******************************************************************************
+  * @file    comp.c
+  * @brief   This file provides code for the configuration
+  *          of the COMP instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "comp.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+COMP_HandleTypeDef hcomp2;
+COMP_HandleTypeDef hcomp6;
+
+/* COMP2 init function */
+void MX_COMP2_Init(void)
+{
+
+  hcomp2.Instance = COMP2;
+  hcomp2.Init.InvertingInput = COMP_INVERTINGINPUT_DAC1_CH2;
+  hcomp2.Init.NonInvertingInput = COMP_NONINVERTINGINPUT_IO1;
+  hcomp2.Init.Output = COMP_OUTPUT_NONE;
+  hcomp2.Init.OutputPol = COMP_OUTPUTPOL_NONINVERTED;
+  hcomp2.Init.BlankingSrce = COMP_BLANKINGSRCE_NONE;
+  hcomp2.Init.TriggerMode = COMP_TRIGGERMODE_NONE;
+  if (HAL_COMP_Init(&hcomp2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+/* COMP6 init function */
+void MX_COMP6_Init(void)
+{
+
+  hcomp6.Instance = COMP6;
+  hcomp6.Init.InvertingInput = COMP_INVERTINGINPUT_DAC1_CH2;
+  hcomp6.Init.NonInvertingInput = COMP_NONINVERTINGINPUT_IO1;
+  hcomp6.Init.Output = COMP_OUTPUT_NONE;
+  hcomp6.Init.OutputPol = COMP_OUTPUTPOL_NONINVERTED;
+  hcomp6.Init.BlankingSrce = COMP_BLANKINGSRCE_NONE;
+  hcomp6.Init.TriggerMode = COMP_TRIGGERMODE_NONE;
+  if (HAL_COMP_Init(&hcomp6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+void HAL_COMP_MspInit(COMP_HandleTypeDef* compHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(compHandle->Instance==COMP2)
+  {
+  /* USER CODE BEGIN COMP2_MspInit 0 */
+
+  /* USER CODE END COMP2_MspInit 0 */
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**COMP2 GPIO Configuration
+    PA7     ------> COMP2_INP
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN COMP2_MspInit 1 */
+
+  /* USER CODE END COMP2_MspInit 1 */
+  }
+  else if(compHandle->Instance==COMP6)
+  {
+  /* USER CODE BEGIN COMP6_MspInit 0 */
+
+  /* USER CODE END COMP6_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**COMP6 GPIO Configuration
+    PB11     ------> COMP6_INP
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN COMP6_MspInit 1 */
+
+  /* USER CODE END COMP6_MspInit 1 */
+  }
+}
+
+void HAL_COMP_MspDeInit(COMP_HandleTypeDef* compHandle)
+{
+
+  if(compHandle->Instance==COMP2)
+  {
+  /* USER CODE BEGIN COMP2_MspDeInit 0 */
+
+  /* USER CODE END COMP2_MspDeInit 0 */
+
+    /**COMP2 GPIO Configuration
+    PA7     ------> COMP2_INP
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_7);
+
+  /* USER CODE BEGIN COMP2_MspDeInit 1 */
+
+  /* USER CODE END COMP2_MspDeInit 1 */
+  }
+  else if(compHandle->Instance==COMP6)
+  {
+  /* USER CODE BEGIN COMP6_MspDeInit 0 */
+
+  /* USER CODE END COMP6_MspDeInit 0 */
+
+    /**COMP6 GPIO Configuration
+    PB11     ------> COMP6_INP
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_11);
+
+  /* USER CODE BEGIN COMP6_MspDeInit 1 */
+
+  /* USER CODE END COMP6_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
