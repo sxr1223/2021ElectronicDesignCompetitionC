dadc311ca74e72b8f0949982b43f6233e20d5444
sxr1223 <1173368667@qq.com>
2022-07-27 16:03:41 +0800

    还没修改完毕

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -50,81 +50,26 @@
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 #define SPWM_NUM 400
-#define DATA_LEN 20
+#define DATA_LEN 3
 #define DATA_CH_NUM 6
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-static uint16_t spwm[]=
-{
-11520,11684,11849,12013,12178,12342,12506,12670,12833,12996,
-13159,13321,13483,13645,13806,13966,14126,14285,14443,14601,
-14758,14914,15069,15224,15377,15530,15682,15832,15982,16130,
-16277,16423,16568,16712,16854,16995,17135,17273,17410,17546,
-17679,17812,17943,18072,18200,18326,18450,18573,18694,18813,
-18930,19045,19159,19271,19381,19489,19594,19698,19800,19900,
-19998,20094,20187,20279,20368,20455,20540,20623,20703,20781,
-20857,20931,21002,21071,21138,21202,21264,21323,21380,21434,
-21487,21536,21583,21628,21670,21710,21747,21782,21814,21843,
-21870,21895,21917,21936,21953,21967,21979,21988,21994,21998,
-22000,21998,21994,21988,21979,21967,21953,21936,21917,21895,
-21870,21843,21814,21782,21747,21710,21670,21628,21583,21536,
-21487,21434,21380,21323,21264,21202,21138,21071,21002,20931,
-20857,20781,20703,20623,20540,20455,20368,20279,20187,20094,
-19998,19900,19800,19698,19594,19489,19381,19271,19159,19045,
-18930,18813,18694,18573,18450,18326,18200,18072,17943,17812,
-17679,17546,17410,17273,17135,16995,16854,16712,16568,16423,
-16277,16130,15982,15832,15682,15530,15377,15224,15069,14914,
-14758,14601,14443,14285,14126,13966,13806,13645,13483,13321,
-13159,12996,12833,12670,12506,12342,12178,12013,11849,11684,
-11520,11355,11190,11026,10861,10697,10533,10369,10206,10043,
-9880,9718,9556,9394,9233,9073,8913,8754,8596,8438,
-8281,8125,7970,7815,7662,7509,7357,7207,7057,6909,
-6762,6616,6471,6327,6185,6044,5904,5766,5629,5493,
-5360,5227,5096,4967,4839,4713,4589,4466,4345,4226,
-4109,3994,3880,3768,3658,3550,3445,3341,3239,3139,
-3041,2945,2852,2760,2671,2584,2499,2416,2336,2258,
-2182,2108,2037,1968,1901,1837,1775,1716,1659,1605,
-1552,1503,1456,1411,1369,1329,1292,1257,1225,1196,
-1169,1144,1122,1103,1086,1072,1060,1051,1045,1041,
-1040,1041,1045,1051,1060,1072,1086,1103,1122,1144,
-1169,1196,1225,1257,1292,1329,1369,1411,1456,1503,
-1552,1605,1659,1716,1775,1837,1901,1968,2037,2108,
-2182,2258,2336,2416,2499,2584,2671,2760,2852,2945,
-3041,3139,3239,3341,3445,3550,3658,3768,3880,3994,
-4109,4226,4345,4466,4589,4713,4839,4967,5096,5227,
-5360,5493,5629,5766,5904,6044,6185,6327,6471,6616,
-6762,6909,7057,7207,7357,7509,7662,7815,7970,8125,
-8281,8438,8596,8754,8913,9073,9233,9394,9556,9718,
-9880,10043,10206,10369,10533,10697,10861,11026,11190,11355
-};
-//max duty 91.2
-//medium 1020
-//amplitude ?
 
 uint16_t adc_data[DATA_LEN][DATA_CH_NUM]={0};
 uint16_t adc_data_fin[DATA_CH_NUM]={0};
 
-float adc_ch1_amp=0;
-uint16_t adc_ch1_max=0;
-uint16_t adc_ch1_min=4096;
-
-uint32_t sys_tic=0;
-
-pid_type_def amp_pid;
-const fp32 amp_p_i_d[3]={0,0.01,0};
+pid_type_def pfc_pid;
+const fp32 pfc_p_i_d[3]={0,0.01,0};
 
 uint16_t vol_to_spwm;
 float vol_set=10;
-float rms;
-float vol_sq_sum=0;
-uint16_t vol_sq_times=0;
 
-uint16_t cycle_extern=20000;
-
-static uint16_t spwm_index=0;
+int32_t vol_in;
+int32_t curr_in;
+uint16_t vol_in_polar=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -140,10 +85,65 @@ void set_PWM(uint16_t dutyA,uint16_t dutyB)
 	hhrtim1.Instance->sTimerxRegs[1].CMP1xR = 23040/2-dutyB/2;
 	hhrtim1.Instance->sTimerxRegs[1].CMP2xR = 23040/2+dutyB/2;
 }
+void set_slow_H(void)
+{
+	HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_RESET);
+	{
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+	}
+	HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_SET);
+}
+
+void set_slow_L(void)
+{
+	HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_RESET);
+	{
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+	}
+	HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_SET);
+}
 
 //#define test
 #ifdef test
-uint32_t test_pwm=21040;
+uint32_t test_pwm=0;
 #endif
 //22000
 //900
@@ -152,102 +152,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	static float x;
 	if(htim==(&htim16))//1k,1ms
 	{
-		sys_tic++;
-		if(sys_tic%(cycle_extern*5/1000)==0)
-		{
-			adc_ch1_max=0;
-			adc_ch1_min=4096;
-			
-			vol_sq_sum=vol_sq_sum/vol_sq_times;
-			x=sqrt(vol_sq_sum);
-//			rms=-1.77028f*x*x + 43.33845f*x + 0.05275f ;
-			rms=43.35312f*x + 0.17551f ;
-//			rms=-28.61181f*x*x*x*x + 31.61418f*x*x*x - 13.79850f*x*x+ 45.18009f*x - 0.04284f;
-			vol_sq_times=vol_sq_sum=0;
-			
-			PID_calc(&amp_pid,rms,vol_set);
-
-			amp_pid.out=1;
-			if(amp_pid.out<0)
-				amp_pid.out=0;
-		}
 	}
 
 	if(htim==(&htim17))
 	{
-		if(spwm_index<200)
-		{
-#ifdef test
-			set_PWM(test_pwm,test_pwm);
-#else
-			set_PWM(spwm[spwm_index]*amp_pid.out,spwm[200+spwm_index]*amp_pid.out);
-#endif
-			if(spwm_index==0)
-			{
-				HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_RESET);
-				{
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-				}
-				HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_SET);
-			}
-		}
-		if(spwm_index>=200)
-		{
-#ifdef test
-			set_PWM(test_pwm,test_pwm);
-#else
-			set_PWM(spwm[spwm_index]*amp_pid.out,spwm[spwm_index-200]*amp_pid.out);
-#endif
-			if(spwm_index==200)
-			{
-				HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_RESET);
-				{
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-					__NOP();
-				}
-				HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_SET);
-			}
-		}
-		spwm_index++;
-		if(spwm_index==400)
-			spwm_index=0;
 	}
 	
 
@@ -279,55 +187,29 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 			adc_data_fin[i]=(sum-max-min)/(DATA_LEN-2);
 		}
 		
-		if(adc_data_fin[0]>adc_ch1_max)
-			adc_ch1_max=adc_data_fin[0];
-		if(adc_data_fin[0]<adc_ch1_min)
-			adc_ch1_min=adc_data_fin[0];
 		
-		adc_ch1_amp=(adc_ch1_max-adc_ch1_min)/4096.0f*3.3f*scale;	
+		vol_in=1895-adc_data_fin[0];
+		curr_in=adc_data_fin[1]-1937;
 		
-		vol_sq_sum+=pow(((adc_data_fin[0]-1864)/4096.0f*3.3f*scale),2);
-		vol_sq_times++;
+		if(vol_in_polar==0&&vol_in>30)
+		{
+			vol_in_polar=1;
+			set_slow_L();
+			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+		}
+		
+		if(vol_in_polar==1&&vol_in<30)
+		{
+			vol_in_polar=0;
+			set_slow_H();
+			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+		}
+		
+		PID_calc(&pfc_pid,vol_in,vol_set);
 		
 		HAL_ADC_Start_DMA(&hadc1,(uint32_t*)adc_data,DATA_LEN*DATA_CH_NUM);
 	}
 }
-#define EXTERN_CYCLE_SAMP_TIMES 10	
-//us
-uint16_t exter_cycle_buff[EXTERN_CYCLE_SAMP_TIMES]={0};
-uint8_t exter_cycle_buff_index=0;
-uint32_t exter_cycle_buff_sum;
-void HAL_COMP_TriggerCallback(COMP_HandleTypeDef *hcomp)
-{	
-	//全部都是用来计算外部信号周期的变量
-
-	if(hcomp==&hcomp2)
-	{		
-		if(__HAL_TIM_GetCounter(&htim15)>2000)
-		{
-			HAL_TIM_Base_Stop(&htim15);
-			
-			//HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_6);
-			if(__HAL_TIM_GetCounter(&htim15)>1*1000)
-				exter_cycle_buff[exter_cycle_buff_index++]=__HAL_TIM_GetCounter(&htim15);
-
-			if(exter_cycle_buff_index==EXTERN_CYCLE_SAMP_TIMES)
-			{
-				for(exter_cycle_buff_index=0;exter_cycle_buff_index<EXTERN_CYCLE_SAMP_TIMES;exter_cycle_buff_index++)
-					exter_cycle_buff_sum+=exter_cycle_buff[exter_cycle_buff_index];
-				cycle_extern=exter_cycle_buff_sum/EXTERN_CYCLE_SAMP_TIMES*2;
-				exter_cycle_buff_sum=0;
-				exter_cycle_buff_index=0;
-				
-				__HAL_TIM_SetAutoreload(&htim17,cycle_extern*72/400-1);
-				//spwm_index=0;
-			}
-			__HAL_TIM_SetCounter(&htim15,0);
-			HAL_TIM_Base_Start(&htim15);
-		}
-	}
-}
-
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -375,7 +257,7 @@ int main(void)
   MX_TIM15_Init();
   /* USER CODE BEGIN 2 */
   
-	PID_init(&amp_pid,PID_DELTA,amp_p_i_d,1,0.4);
+	PID_init(&pfc_pid,PID_DELTA,pfc_p_i_d,1,0.4);
 	
 	HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_MASTER);
 	
@@ -403,8 +285,8 @@ int main(void)
 //	OLED_Display_On();
 //	OLED_printf(0,0,16,"Hello World.");
 //	
-	HAL_DAC_SetValue(&hdac1,DAC_CHANNEL_2,DAC_ALIGN_12B_R,2000);
-	HAL_DAC_Start(&hdac1,DAC_CHANNEL_2);
+//	HAL_DAC_SetValue(&hdac1,DAC_CHANNEL_2,DAC_ALIGN_12B_R,2000);
+//	HAL_DAC_Start(&hdac1,DAC_CHANNEL_2);
 	
 //	HAL_COMP_Start_IT(&hcomp2);
 //	HAL_TIM_Base_Start(&htim15);
