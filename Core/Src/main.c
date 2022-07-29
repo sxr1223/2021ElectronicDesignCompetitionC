0337f221a31d2bb90b31d08243ae787c414b4434
sxr1223 <1173368667@qq.com>
2022-07-29 13:49:44 +0800

    加了软过零，mode 1->mode 2过零有些问题

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -50,7 +50,7 @@
 #define DATA_LEN 5
 #define DATA_CH_NUM 6
 
-uint16_t MAX_PWM_DELT=100;
+uint16_t MAX_PWM_DELT=500;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -69,8 +69,8 @@ float vol_set=10;
 int32_t vol_in;
 int32_t curr_in;
 int8_t vol_in_polar=0;
-int8_t vol_in_polar_last=0;
-
+int8_t vol_in_polar_last=1;
+uint16_t target_pwm;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -79,66 +79,22 @@ void SystemClock_Config(void);
 
 void set_PWM(uint16_t dutyA,uint16_t dutyB)
 {
-		if((TIM1->CCR2-dutyA)>MAX_PWM_DELT)
-			TIM1->CCR2-=MAX_PWM_DELT;
-		else if((dutyA-TIM1->CCR2)>MAX_PWM_DELT)
-			TIM1->CCR2+=MAX_PWM_DELT;
-		else
+//		if((TIM1->CCR2-dutyA)>MAX_PWM_DELT)
+//			TIM1->CCR2-=MAX_PWM_DELT;
+//		else if((dutyA-TIM1->CCR2)>MAX_PWM_DELT)
+//			TIM1->CCR2+=MAX_PWM_DELT;
+//		else
 			TIM1->CCR2=dutyA;
 }
 void set_slow_H(void)
 {
 	HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_RESET);
-	{
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-	}
 	HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_SET);
 }
 
 void set_slow_L(void)
 {
 	HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin,GPIO_PIN_RESET);
-	{
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-	}
 	HAL_GPIO_WritePin(SLOW_L_GPIO_Port,SLOW_L_Pin,GPIO_PIN_SET);
 }
 
@@ -179,53 +135,69 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 		vol_in=1880-adc_data_fin[0];
 		curr_in=adc_data_fin[2]-1838;
 		
-		if(vol_in<20&&vol_in>-20)
+		//*********************** mode 0 ***********************
+		if(vol_in<50&&vol_in>-50&&(vol_in_polar==1||vol_in_polar==-1))
 		{
 			HAL_TIM_PWM_Stop(&htim1,TIM_CHANNEL_2);
 			HAL_TIMEx_PWMN_Stop(&htim1,TIM_CHANNEL_2);
-			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3|GPIO_PIN_4,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(SLOW_H_GPIO_Port,SLOW_H_Pin|SLOW_L_Pin,GPIO_PIN_RESET);
 			
-			vol_in_polar_last=vol_in_polar;
 			vol_in_polar=0;
 			
-			if(vol_in_polar_last==-1)
-				TIM1->CCR2=7199;
-			if(vol_in_polar_last==1)
-				TIM1->CCR2=0;
+//			if(vol_in_polar_last==-1)
+//				TIM1->CCR2=7199;
+//			if(vol_in_polar_last==1)
+//				TIM1->CCR2=0;
+			target_pwm=7199-TIM1->CCR2;
 		}
-		else
+		//*********************** mode 1 ***********************
+		if(vol_in_polar==-2&&vol_in>20&&vol_in_polar_last==-1&&(TIM1->CCR2>target_pwm))
 		{
-			if(vol_in_polar==0&&vol_in>10)
-			{
-				HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
-				HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_2);
+			HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+			HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_2);
 
-				vol_in_polar_last=vol_in_polar;
-				vol_in_polar=1;
-				//HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
-				set_slow_L();
-			}
+			vol_in_polar_last=1;
+			vol_in_polar=1;
+			//HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_SET);
+			set_slow_L();
+		}
+		//*********************** mode -1 ***********************
+		if(vol_in_polar==-2&&vol_in<-20&&vol_in_polar_last==1&&(TIM1->CCR2<target_pwm))
+		{
+			HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+			HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_2);
 			
-			if(vol_in_polar==1&&vol_in<-10)
+			vol_in_polar_last=-1;
+			vol_in_polar=-1;
+			//HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
+			set_slow_H();
+		}
+		//*********************** mode -2 ***********************
+		if(vol_in_polar==0&&(vol_in<-50||vol_in>50))
+		{
+			vol_in_polar=-2;
+
+			if(vol_in_polar_last==-1)
 			{
-				HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+				target_pwm=6999;
 				HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_2);
 				
-				vol_in_polar_last=vol_in_polar;
-				vol_in_polar=0;
-				//HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,GPIO_PIN_RESET);
-				set_slow_H();
+				//HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+			}
+			if(vol_in_polar_last==1)
+			{
+				target_pwm=2000;
+				HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+				
 			}
 		}
-		
-		
-		
-		
+	
 		if(vol_in_polar==1)//pwm cannot too small
 		{
 			PID_calc(&pfc_pid,curr_in,vol_in);
 			if(pfc_pid.out<0)
-				pfc_pid.out=0;
+				pfc_pid.out=2000;
+			pfc_pid.out=2000;
 			set_PWM(7199-pfc_pid.out,0);
 		}
 
@@ -233,19 +205,47 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 		{
 			PID_calc(&pfc_pid,curr_in,vol_in);
 			if(pfc_pid.out<0)
-				pfc_pid.out=0;
+				pfc_pid.out=2000;
+			pfc_pid.out=2000;
 			set_PWM(pfc_pid.out,0);
 		}
 		
-		if(vol_in_polar==0)
+		if(vol_in_polar==-2)
 		{
 			if(vol_in_polar_last==1)
-				set_PWM(6000,0);
-			
+			{
+//				if(TIM1->CCR2+MAX_PWM_DELT>7199)
+//					TIM1->CCR2=7199;
+//				else
+//					TIM1->CCR2+=MAX_PWM_DELT;
+				if(TIM1->CCR2-MAX_PWM_DELT<0)
+					TIM1->CCR2=0;
+				else
+					TIM1->CCR2-=MAX_PWM_DELT;
+			}
 			if(vol_in_polar_last==-1)
-				set_PWM(7000,0);
-			
+			{
+				if(TIM1->CCR2-MAX_PWM_DELT<0)
+					TIM1->CCR2=0;
+				else
+					TIM1->CCR2-=MAX_PWM_DELT;
+				
+//				if(TIM1->CCR2+MAX_PWM_DELT>7199)
+//					TIM1->CCR2=7199;
+//				else
+//					TIM1->CCR2+=MAX_PWM_DELT;
+			}
 		}
+		
+//		if(vol_in_polar==0)
+//		{
+//			if(vol_in_polar_last==1)
+//				set_PWM(6000,0);
+//			
+//			if(vol_in_polar_last==-1)
+//				set_PWM(7000,0);
+//			
+//		}
 		
 		HAL_ADC_Start_DMA(&hadc1,(uint32_t*)adc_data,DATA_LEN*DATA_CH_NUM);
 	}
